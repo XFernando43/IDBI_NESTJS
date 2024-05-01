6665e1a9fc9ca92b5f81cfd16351f2161d34b16a
XFernando43 <U20171E997@upc.edu.pe>
2024-05-01 15:28:01 -0500

    prueba

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+   // prueba
 
   // recordar usar como etiqueta en los controller no en el main da problemas
   
