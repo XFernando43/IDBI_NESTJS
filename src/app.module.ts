d113c6c30d0ae169d95572f269e79af642f4a75f
XFernando43 <U20171E997@upc.edu.pe>
2024-04-30 19:12:38 -0500

    add: DB configuration added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CustomerModule } from './customer/customer.module';
-import { AccountModule } from './account/account.module';
+import { CustomerModule } from './Authentication-Managment/Infracstruture/customer.module';
+import { AccountModule } from './Authentication-Managment/Infracstruture/account.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [CustomerModule, AccountModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456789',
+      database: 'idbi',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, 
+      logging:true, 
+    }),    
+    CustomerModule, 
+    AccountModule],
   controllers: [AppController],
   providers: [AppService],
 })
